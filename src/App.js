c5dcc74df8760ad78374dac0d947e80671d45587
Melanie Mendoza <melanie.mendoza@gmail.com>
2020-07-24 06:37:12 -0400

    Added header, link, and route in App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 import React from 'react';
 import './App.css';
+import { Route, Link } from 'react-router-dom';
+import Register from './components/Register';
 
 function App() {
-	return <div className='App'></div>;
+	return (
+		<div className='App'>
+      <nav>
+        <a href="#">Profile</a>
+        <a href="#">About</a>
+        <a href="#">Login</a>
+      </nav>
+      <header className="App-header">
+        <h1>Welcome to Essentialism. <br></br> Get more things done with less.</h1> 
+        <Link to={'/register'}
+          className='register-link'
+        >
+          Sign Up
+        </Link>
+      </header>
+      <Route path='/register'>
+        <Register />
+      </Route>
+		</div>
+	);
 }
 
 export default App;
