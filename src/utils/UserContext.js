9f9247b0f8380f9541381e47dcc854ffcfee07fb
Michelle Hey <miscmiche@gmail.com>
2020-07-27 20:17:35 -0400

    UserContext.js edits

--- src/utils/UserContext.js
+++ src/utils/UserContext.js
@@ -1,5 +1,19 @@
 import { createContext } from 'react';
 
 
+//import as:
+// import { UserContext } from './utils/UserContext';
+
+
+
+// where used, wrap in the UserContext.Provider tag
+
+// return (
+//     <UserContext.Provider value={user}>
+//         <div className="container">
+//             <User />
+//         </div>
+//     </UserContext.Provider>
+// );
 
 export const UserContext = createContext();
