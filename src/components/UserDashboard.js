d650a7fadf9e5a63798be321817a780319491fab
Michelle Hey <miscmiche@gmail.com>
2020-07-28 20:02:02 -0400

    Made form for projects to be displayed in dashboard after endpoitns completed.

--- src/components/UserDashboard.js
+++ src/components/UserDashboard.js
@@ -1,10 +1,15 @@
 import React from "react";
+import ValueCard from './ValueCard';
 
 // api will be used to pull and render user information, or we can store it into a usercontext
+// will be a private route, along with onboarding
 
 export default function UserDashboard() {
     return (
-        <div>
+        <div className="dashboard-container">
+            {/* I want to display username, values, and projects */}
+            <ValueCard />
+            {/* <Projects /> */}
 
         </div>
     )
