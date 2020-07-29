44659859ae043f28bd1b5bd85c25a1f2ed6164ac
Michelle Hey <miscmiche@gmail.com>
2020-07-28 20:30:33 -0400

    Small changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom';
 const Home = () => {
   return (
     <div className='home'>
-      <div className='nav'>
-        <Link to={'/login'}
+      <div className='nav'> 
+        <Link to={'/login'} 
           className='userlogin-link'
         >
           Login
@@ -15,7 +15,7 @@ const Home = () => {
         </Link>
       </div>
       <header className="App-header">
-        <h1>Live Happier with Essentialism.</h1>
+        <h1>Live Happier with Essentialism.</h1> 
         <Link to={'/register'}
           className='register-link'
         >
