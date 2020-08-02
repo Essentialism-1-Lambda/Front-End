e23cdf40036c67bb02795bed501ca9d7346cde96
Dellrodar <3357168+Dellrodar@users.noreply.github.com>
2020-08-02 19:23:44 -0400

    More bug Fixes * Moved user onboarding check into header * Added verbage into final stepper FinalForm * Removed user check from dashboard as it caused a rendering error.

--- src/components/Nav/Header.js
+++ src/components/Nav/Header.js
@@ -1,13 +1,13 @@
 import React, { useContext } from 'react';
 import { useHistory, Link } from 'react-router-dom';
-import {Button} from '@material-ui/core';
+import { Button } from '@material-ui/core';
 import { UserContext } from '../../Context/UserContext';
 
 export const Header = () => {
-  const history = useHistory();
-	const {isAuth, setAuth, setUser} = useContext(UserContext);
+	const history = useHistory();
+	const { isAuth, setAuth, setUser } = useContext(UserContext);
 
-  const logout = (e) => {
+	const logout = (e) => {
 		e.preventDefault();
 		setAuth(null);
 		setUser({});
@@ -16,41 +16,60 @@ export const Header = () => {
 		history.push('/');
 	};
 
-  return (
-    <div className='nav-bar'>
-      {isAuth ? (
-        <>
-          <section className='private-nav'>
-            <div className='links'>
-              <Link className="userlogin-link" to={"/dashboard"}>
-                Dashboard
-              </Link>
-              <Link className="userlogin-link" to={"/onboarding"}>
-                Change Values
-              </Link>
-              <Link className="userlogin-link" to={{ pathname: "https:/essentialism-1-lambda.github.io/Marketing/" }} target="_blank">
-              About Us
-              </Link>
-              <Button color='primary' type='submit' onClick={logout}>
-                Log Out
-            </Button>
-            </div>
-          </section>
-        </>
-      ) : (
-          <section className='public-nav'>
-            <Link className="userlogin-link" to={"/"}>
-              Home
-              </Link>
-            <Link className="userlogin-link" to={"/login"}>
-              Login
-            </Link>
-            <Link className="userlogin-link" to={{ pathname: "https:/essentialism-1-lambda.github.io/Marketing/" }} target="_blank">
-              About Us
-            </Link>
-          </section>
-        )}
-    </div>
-  );
-}
+	return (
+		<div className='nav-bar'>
+			{isAuth ? (
+				<>
+					{(user) => {
+						if (user !== null) {
+							if (user.values && user.values.length === 0) {
+								history.push('/onboarding');
+							} else {
+								history.push('/dashboard');
+							}
+						}
+					}}
+					<section className='private-nav'>
+						<div className='links'>
+							<Link className='userlogin-link' to={'/dashboard'}>
+								Dashboard
+							</Link>
+							<Link className='userlogin-link' to={'/onboarding'}>
+								Change Values
+							</Link>
+							<Link
+								className='userlogin-link'
+								to={{
+									pathname: 'https:/essentialism-1-lambda.github.io/Marketing/',
+								}}
+								target='_blank'>
+								About Us
+							</Link>
+							<Button color='primary' type='submit' onClick={logout}>
+								Log Out
+							</Button>
+						</div>
+					</section>
+				</>
+			) : (
+				<section className='public-nav'>
+					<Link className='userlogin-link' to={'/'}>
+						Home
+					</Link>
+					<Link className='userlogin-link' to={'/login'}>
+						Login
+					</Link>
+					<Link
+						className='userlogin-link'
+						to={{
+							pathname: 'https:/essentialism-1-lambda.github.io/Marketing/',
+						}}
+						target='_blank'>
+						About Us
+					</Link>
+				</section>
+			)}
+		</div>
+	);
+};
 export default Header;
