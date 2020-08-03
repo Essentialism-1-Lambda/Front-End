08d0bfc6db63aa3962db153435b7a4b68f14dc02
Michelle Hey <miscmiche@gmail.com>
2020-08-03 16:11:23 -0400

    Changes to project list for demo api calls.

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,58 +1,65 @@
 import React, { useState, useEffect } from "react";
-import { axiosWithAuth } from '../utils/AxiosWithAuth';
-import { userData } from '../DataStubs/data';
+// import { axiosWithAuth } from '../utils/AxiosWithAuth';
+import { userData, projectExample } from '../DataStubs/data';
 import "../index.css";
 
 // if context were used vs dummy data
+
 // import React, { UseContext } from 'react';
 // import { ProjectContext } from '../Context/ProjectContext';
 // import { UserContext } from '../Context/UserContext';
-    // const ProjConsumer = ProjContext.Consumer;
+    
+// const ProjConsumer = ProjContext.Consumer;
 
-// Users
-// GET /api/users
-// GET /api/users/(ID)
-// Delete /api/users/(ID)
-// Update PUT /api/users/(ID)
-// Projects
-// GET /api/users/(ID)/project
-// GET (by project id) /api/users/(ID)/project/( ProjectID )
-// update /api/users/(ID)/project/( ProjectID )
-// Delete /api/users/(ID)/project/( ProjectID )
-// Values
-// GET /api/values
-// GET ( users value by ID ) /api/users/(ID)/values/( ValuesID )
-// Update /api/users/(ID)/values/( ValuesID )
-// Delete /api/users/(ID)/values/( ValuesID )
+    // Users
+    // GET /api/users
+    // GET /api/users/(ID)
+    // Delete /api/users/(ID)
+    // Update PUT /api/users/(ID)
+
+    // Projects
+    // GET /api/users/(ID)/project
+    // GET (by project id) /api/users/(ID)/project/( ProjectID )
+    // update /api/users/(ID)/project/( ProjectID )
+    // Delete /api/users/(ID)/project/( ProjectID )
+
+    // Values
+    // GET /api/values
+    // GET ( users value by ID ) /api/users/(ID)/values/( ValuesID )
+    // Update /api/users/(ID)/values/( ValuesID )
+    // Delete /api/users/(ID)/values/( ValuesID )
 
 
 
 const ProjectList = () => {
     const { push } = useHistory();
-    const [listState, setListState] =useState([]);
+    const [listState, setListState] = useState(userData);
+    const [user, setUser] = useState(userData);
+
 
     const deleteProject = e => {
         e.preventDefault();
-        axiosWithAuth()
-          .delete(`https://essentialism-bw.herokuapp.com/api/user/${id}/projects/${project.id}`)
-            .then(response => {
-                console.log('deleted project', response);
-                setListState(listState.filter(item => `${userData.projects.name}` !== userData.projects.name));
-                push("/Projects")
-            })
-            .catch(err => console.log(err));
+        // axiosWithAuth()
+        //   .delete(`users/${id}/projects/${project.id}`)
+        //     .then(response => {
+        //         console.log('deleted project', response);
+        //         setListState(listState.filter(item => `${userData.projects.name}` !== userData.projects.name));
+        //         push("/Projects")
+        //     })
+        //     .catch(err => console.log(err));
       };
 
 
-    useEffect(() => {
-        axiosWithAuth()
-            .get("https://essentialism-bw.herokuapp.com/api/user/${id}/projects")
-            .then(response => {
-                setListState(response.data);
-                console.log('list ', response.data);
-        })
-        .catch(err => console.log(err));
-    }, [])
+    // useEffect(() => {
+    //     axiosWithAuth()
+    //         .get(`users/${id}/projects`)
+    //         .then(response => {
+    //           console.log('project list', response);
+    //           setListState(response.data);
+    //           // push("/Projects")
+    //     })
+    //     .catch(err => console.log(err));
+    // }, [])
 
 
     return(
